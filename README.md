4a81bd95b8687876b38e4c27d356314ede8645a3
Becca Burke <reburke286@gmail.com>
2019-10-05 21:47:58 -0700

    finalized js for submission

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # PasswordGenerator
+Week 3 Homework 
+
+Description:
+This is an application that generates a random password based on user input. It allows the user to copy to clipboard as well. It uses responsive design from bootstrap, eventListeners for buttons, DOM manipulation, and javascript functions like math.floor and math.random.
+
+Usage:
+This is intended to work for a user who needs a password created, ideally a strong password that's difficult to hack.
+
+Credits:
+1) getbootstrap.com
+
+Tests:
+console.log's throughout js to show output for confirms and password creation 
--- script.js
+++ script.js
@@ -1,19 +1,66 @@
 var button = document.querySelector('#generate');
+var textArea = document.querySelector('#exampleFormControlTextarea1');
+var copyToClipboardBtn = document.querySelector('#copyToClipboardBtn');
+let howLong = '';
+var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-'];
+var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+'y', 'z'];
+var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L',
+'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+'Y', 'Z'];
+var numChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function copyClipboard(){
+  var el = document.createElement('textarea');
+  document.body.appendChild(el);
+  el.select(); 
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  alert("Copied the text: " + textArea.copyText.value);
+};
+
 button.addEventListener('click', function(){
-   // var howLong = prompt('How long would you like your password? Please pick a number between 8 and 128.');
-    //console.log(howLong);
-    var howLong = null
-   
     while (!(howLong >=8 && howLong <=128)) {
-      howLong = prompt('How long would you like your password? Please pick a number between 8 and 128.') 
+     var howLong = prompt('How long would you like your password? Please pick a number between 8 and 128.') 
     };
-    console.log(howLong);
+    console.log(howLong); //value from prompt
     
-    var userSpecChar = confirm('Would you like to use special characters?');
-    console.log(userSpecChar);     
-})
+    var shouldHaveSpecialCharacters = confirm('Would you like to use special characters?');
+    console.log(shouldHaveSpecialCharacters);  //true or false
+    var shouldBeUppercase = confirm('Would you like to use upper case characters?');
+    console.log(shouldBeUppercase); //true or false
+    var shouldBeLowerCase = confirm('Would you like to use lower case characters?');
+    console.log(shouldBeLowerCase); //true or false
+    var shouldHaveNumbers = confirm('Would you you to use numbers?');
+    console.log(shouldHaveNumbers); //true or false
 
-var specChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(']
+    var allPossibleCharacters = '';
+    if (shouldHaveSpecialCharacters) {
+      allPossibleCharacters = allPossibleCharacters + specialChar; 
+    } //will add specialChar to string if true
+    if (shouldBeUppercase) {
+      allPossibleCharacters = allPossibleCharacters + upperCaseChar;
+    } //will add upperCaseChar to string if true
+    if (shouldBeLowerCase) {
+     allPossibleCharacters = allPossibleCharacters + lowerCaseChar;
+    } //will add lowerCaseChar to string if true
+    if (shouldHaveNumbers) {
+      allPossibleCharacters = allPossibleCharacters + numChar;
+    } //will add numChar to string if true
+    else {
+      alert('Please choose at least one type of character for your password')
+    };
+      for (i = 0; i < howLong; i++) {
+      var randomIndex = Math.floor(Math.random() * allPossibleCharacters.length);
+      var randomChar = allPossibleCharacters[randomIndex];
+      var password = password + randomChar;
+      console.log(password);
+      document.createElement("textarea").value = password; 
+    } 
+  
+});
 
+copyToClipboardBtn.addEventListener('click', copyClipboard(textArea));
 
-
+  
